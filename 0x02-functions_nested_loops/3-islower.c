f0a6718fe2390c4131148215ffa4f4acef2e2f7c
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-02-21 06:16:41 -0800

    5. Sign

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _islower - find small letters
+ * @c: letter to be checked
  * Return: 0 or 1
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z') {
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	} else {
+	 else
 		return (0);
-	}
+	
 }
