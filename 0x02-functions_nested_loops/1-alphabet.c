55e62d367c1e56bc1ea49eb63379b6eb6b04d2d7
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-02-21 12:11:46 -0800

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@ void print_alphabet(void)
 {
 	char c;
 
-        c = 'a';
-        while
-                (c <= 'z') {
-                        _putchar(c);
-                        c++;
-                }
-        _putchar('\n');
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
