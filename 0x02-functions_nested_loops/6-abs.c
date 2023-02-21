3d82e2f20c992644000a2b51d3fecd1ca7291bc7
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-02-21 08:15:21 -0800

    7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,10 +4,10 @@
  * @n: value to abs
  * Return: Value as absolute
  */
-int _abs (int n)
+int _abs(int n)
 {
 	if (n < 0)
 		return (-n);
 	else
-		return n;
+		return (n);
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: number to be used
+ * Return: Last digit of n
+ */
+int print_last_digit(int n)
+{
+	return (n%10);
+}
