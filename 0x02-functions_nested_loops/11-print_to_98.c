a0d7d43d46d70a2f917fdcae824224527ff0ada2
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-02-22 05:04:54 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf("%d, ", i);
 			else
-				printf("%d\n");
+				printf("%d\n", i);
 		}
 	} else
 	{
@@ -25,7 +25,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf("%d, ", i);
 			else
-				printf("%d\n");
+				printf("%d\n", i);
 		}
 	}
 }
