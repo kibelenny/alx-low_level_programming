f12757396edc9d7cee809821a3cc3f45b1d93050
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-02-22 05:34:28 -0800

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,39 +10,42 @@ void print_times_table(int n)
 	int j;
 	int product;
 
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			product = i * j;
-			if (j == 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(product + '0');
-			}
+				product = i * j;
+				if (j == 0)
+				{
+					_putchar(product + '0');
+				}
 
-			if (product < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product + '0');
-			} else if (product >= 10 && product <= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			} else if (product >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((product / 100) + '0');
-				_putchar(((product / 10) % 10) + '0');
-				_putchar((product % 10) + '0');
+				if (product < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				} else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				} else if (product >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
