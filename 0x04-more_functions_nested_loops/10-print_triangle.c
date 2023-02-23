6b433955b9f1d62a7b8a0dfef23ca2fc14e07368
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-02-23 05:19:55 -0800

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int row, column;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (row = 0; row < size; row++)
+	{
+		for (column = 0; column < size; column++)
+		{
+			if (column + 1 >= size - row)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
