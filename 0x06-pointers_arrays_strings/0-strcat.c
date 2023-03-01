7e6af93d3179cbb580f84b6a3ed325caa18411ff
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-03-01 04:41:01 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - Concats str to another str
+ * @dest: str to append to
+ * @src: str to aprrend from
+ * Return: appended string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}	
+	dest[i] = '\0';
+	return (dest);
+}
