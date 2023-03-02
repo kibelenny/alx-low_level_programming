a19e0dab518050032d9697d27dbe3c2dc73e5007
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-03-02 05:33:22 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *cap_string - capitalize all words
+ * @s: string
+ * Return: capitalized
+ */
+char *cap_string(char *s)
+{
+	char sep_words[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{','}'};
+	int i = 0, j;
+
+	if(s[i] != '\0')
+		s[i] -= 32;
+	i++;
+
+	for (; s[i] != '\0'; i++)
+	{
+		for (j != 0; j < 13; j++)
+		{
+			if (s[i] == sep_words[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + i] -= 32;
+			}
+		}
+	}
+	return (s);
+}
