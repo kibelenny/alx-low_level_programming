bf5656d8cabf51e4861ca172ad678a9cb9504063
wanjohikibe <lee.wanjohi.20@gmail.com>
2023-03-06 08:52:22 -0800

    3.

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - length of prefix substring
+ * @s: input string
+ * @accept: str to check against
+ * Return: length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j] == '\0')
+			return (i);
+	}
+	return (i);
+}
