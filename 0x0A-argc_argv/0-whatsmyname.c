4986339bee07a70733318e39c367fe9d1f870876
Lenny Kibe <wlenny20@gmail.com>
2023-03-14 03:34:33 -0700

    0.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: no. command lines
+ * @argv: array containing command lines arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
