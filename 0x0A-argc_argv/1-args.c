cbd31a98cd91f69640a91b199c3c97209328d6aa
Lenny Kibe <wlenny20@gmail.com>
2023-03-14 03:55:43 -0700

    1.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print args
+ * @argc: number of command line args
+ * @argv: args in command line
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d", argc);
+	return (0);
+}
