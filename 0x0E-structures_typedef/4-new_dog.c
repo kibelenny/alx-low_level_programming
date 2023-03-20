cc1675e13b491d9fefd548336806f0677bdb15f2
Lenny Kibe <wlenny20@gmail.com>
2023-03-20 09:33:51 -0700

    4.A

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,7 +37,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (i = 0; i < lower; i++)
+	for (i = 0; i < owner_len; i++)
 		p_dog->owner[i] = owner[i];
 
 	p_dog->owner[i] = '\0';
