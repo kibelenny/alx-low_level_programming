a39b6033d769022944188d0492f489220cbaaab4
Lenny Kibe <wlenny20@gmail.com>
2023-03-22 06:56:16 -0700

    4.Most

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_opcodes(int num_bytes);
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes(int num_bytes);
+
+	return (0);
+}
+
+void print_opcodes(int num_bytes)
+{
+	int i;
+	unsigned char *ptr;
+
+	ptr = (unsigned char *) print_opcodes;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", *ptr);
+		ptr++;
+	}
+	printf("\n");
+}
