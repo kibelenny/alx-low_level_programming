48272f8c3154b5ee4000be72685dcf5bf41d2e06
Lenny Kibe <wlenny20@gmail.com>
2023-03-22 05:11:42 -0700

    1.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates over arrays and implements action
+ * @array: array to loop over
+ * @size: size of array
+ * @action: what to do to each element
+ * Return: null
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
