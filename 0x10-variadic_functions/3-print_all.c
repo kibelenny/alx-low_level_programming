df910580c424629d6d8f76c176572fe90a012b65
Lenny Kibe <wlenny20@gmail.com>
2023-03-23 06:22:32 -0700

    3.To

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,7 +19,7 @@ void print_all(const char * const format, ...)
 		j = 0;
 		while (t_arg[j])
 		{
-			if (format[i] == t_arg[j])
+			if (format[i] == t_arg[j] && i != 0)
 			{
 				printf(", ");
 				break;
