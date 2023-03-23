353f16755a406cb883ee962ef82e161d247c38e4
Lenny Kibe <wlenny20@gmail.com>
2023-03-23 06:23:27 -0700

    3.To

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -36,7 +36,7 @@ void print_all(const char * const format, ...)
 				printf("%d", va_arg(valist, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(valist, int));
+				printf("%f", va_arg(valist, float));
 				break;
 			case 's':
 				str = va_arg(valist, char *);
