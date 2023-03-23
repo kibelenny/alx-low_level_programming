cca4f37d9f8eefe36c68757a201ea2c46f128c04
Lenny Kibe <wlenny20@gmail.com>
2023-03-23 06:15:40 -0700

    3.To

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,57 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints everything
+ * @format: list of all arg types passed
+ * Return: Void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j;
+	char *str;
+	const char t_arg[] = "cifs";
+
+	va_start(valist, format);
+	while (format[i])
+	{
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j])
+			{
+				printf(", ");
+				break;
+			}
+			j++;
+		}
+
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(valist, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(valist, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, int));
+				break;
+			case 's':
+				str = va_arg(valist, char *);
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(valist);
+}
+
+	
