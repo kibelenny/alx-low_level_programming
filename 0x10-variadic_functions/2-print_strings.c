f2a399e15ce691b1a5aa94f17429fdb0db5a8627
Lenny Kibe <wlenny20@gmail.com>
2023-03-23 06:02:41 -0700

    One

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 
 		if (i < n - 1 && separator != NULL)
-			printf("%s", seapartor);
+			printf("%s", separator);
 	}
 
 	printf("\n");
