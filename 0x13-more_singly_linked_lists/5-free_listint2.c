e39d12840a9a0919e20ffdea1f388605d98a2915
kibelenny <wlenny20@gmail.com>
2023-04-04 16:06:27 +0300

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
+ * free_listint2 - frees a linked list
  * @head: linked list
  * Return: void
  */
 
-void free_listint(listint_t *head)
+void free_listint2(listint_t *head)
 {
 	listint_t *current = head, *next;
 
