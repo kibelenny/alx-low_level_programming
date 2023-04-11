fc8dcffbb2bc054bead9085cedfbf372a4e78a86
kibelenny <wlenny20@gmail.com>
2023-04-12 00:07:02 +0300

    5. 101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns how many bits we should flip
+ * @n: first number to compare
+ * @m: second number to compare
+ * Return: values to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int res = 0, i;
+	unsigned long int diff = n ^ m;
+
+	for (i = 0; i < 64; i++)
+		res += (diff[i] & 1);
+	return (res);
+}
